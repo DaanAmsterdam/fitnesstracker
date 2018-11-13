4b46457a29c6a414664f1b50ba5ba8064e55e125
DaanAmsterdam <daan227@gmail.com>
2018-11-13 17:54:04 +0100

    conf. git

--- webpack.mix.js
+++ webpack.mix.js
@@ -3,5 +3,4 @@ const tailwindcss = require("tailwindcss");
 
 mix.js('resources/js/app.js', 'public/js')
    .postCss('resources/css/app.css', 'public/css', [tailwindcss('./tailwind.js')]);
-
    mix.browserSync('fitnesstracker.code');
